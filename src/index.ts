b9399fecfc146064d4677dc88f51faef47cd9bd8
Maikel van de Lisdonk <maikel@devhelpr.com>
2019-04-04 15:19:48 +0200

    Exporting all plugins

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import { ReduxActionTask } from './ReduxActionTask';
 import { ReduxArrayTask } from './ReduxArrayTask';
+import { ReduxAssignArrayActionTask } from './ReduxAssignArrayActionTask';
+import { ReduxAssignTask } from './ReduxAssignTask';
+import { ReduxClearArrayActionTask } from './ReduxClearArrayActionTask';
+import { ReduxGetKeyTask } from './ReduxGetKeyTask';
+import { ReduxGetTask } from './ReduxGetTask';
+import { ReduxKeyValueTask } from './ReduxKeyValueTask';
+import { ReduxPushArrayActionTask } from './ReduxPushArrayActionTask';
+import { ReduxSetKeyActionTask } from './ReduxSetKeyActionTask';
+import { ReduxThunkActionTask } from './ReduxThunkActionTask';
+import { StoreObserverTask } from './StoreObserverTask';
 
-export { ReduxArrayTask};
+export {
+	ReduxActionTask,
+	ReduxArrayTask,
+	ReduxAssignArrayActionTask,
+	ReduxAssignTask,
+	ReduxClearArrayActionTask,
+	ReduxGetKeyTask,
+	ReduxGetTask,
+	ReduxKeyValueTask,
+	ReduxPushArrayActionTask,
+	ReduxSetKeyActionTask,
+	ReduxThunkActionTask,
+	StoreObserverTask
+};
